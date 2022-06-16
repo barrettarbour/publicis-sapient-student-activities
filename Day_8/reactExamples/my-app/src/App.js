af3b759719ba4eefb5377a119017b8261f55d262
Barrett Arbour <bararbou@publicisgroupe.net>
2022-06-16 16:17:04 -0400

    day 8 exercises

---
+++ Day_8/reactExamples/my-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App(){
+  const list = [
+    "Pineapple",
+    "Apple",
+    "Banana",
+    "Watermelon",
+    "Kiwi",
+    "Dragonfruit",
+    "Mangosteen"
+  ]
+
+  return (
+    <div>
+      Search: <input type="text"/>
+    </div>
+  )
+}
+
+export default App;
